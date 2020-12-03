285d0e1160bff2855e6283aff0ffd46dd73cc900
Richard Morris <rwmorris.bjj@gmail.com>
2020-12-03 22:09:36 +0000

    working on CLI project

--- expense/expense.rb
+++ expense/expense.rb
@@ -2,38 +2,74 @@
 
 require "pg"
 
-expensesdb = PG.connect(dbname: 'expenses')
-
-@information = expensesdb.exec "SELECT * FROM expenses ORDER BY created_on ASC;"
-
-def list 
-  @information.values.each do |value|
-    puts "#{value[0]} | #{value[3]}| #{value[1].rjust(11)}| #{value[2]}"
+class CLI
+  def initialize
+    @expenses = ExpenseData.new
   end
 
-  sleep (5)
+  def run(argv)
+    user_command = argv.first
+
+    if user_command == 'list' 
+      @expenses.list_expense
+    elsif user_command == 'add'
+      if argv.length == 3
+        @expenses.add_expense(argv[1], argv[2])
+      else 
+        puts "You must provide an amount and memo."
+      end
+    elsif user_command == 'help' || user_command.empty?
+      @expenses.help
+    elsif user_command == 'search'
+      @expenses.search(argv[1])
+    end
+  end
+
 end
 
-def help
-  puts <<~TEXT
-  An expense recording system
 
-  Commands:
+class ExpenseData
+  def initialize 
+    @expensesdb = PG::connect(dbname: 'expenses')
+  end
 
-  add AMOUNT MEMO [DATE] - record a new expense
-  clear - delete all expenses
-  list - list all expenses
-  delete NUMBER - remove expense with id NUMBER
-  search QUERY - list expenses with a matching memo field"
-  TEXT
+  def add_expense (cost, memo_text)
+    @expensesdb.exec_params("INSERT INTO expenses (created_on, amount, memo) VALUES (NOW(), $1, $2)", [cost, memo_text])
+  end
 
-  sleep (5)
+  def list_expense
+    recall_information = @expensesdb.exec "SELECT * FROM expenses ORDER BY created_on ASC;"
+    format_print_result(recall_information)
+  end
+
+  def help
+    puts <<~TEXT
+    An expense recording system
+
+    Commands:
+
+    add AMOUNT MEMO [DATE] - record a new expense
+    clear - delete all expenses
+    list - list all expenses
+    delete NUMBER - remove expense with id NUMBER
+    search QUERY - list expenses with a matching memo field"
+    TEXT
+  end
+
+  def search(search_term)
+    results = @expensesdb.exec_params("SELECT * FROM expenses WHERE memo LIKE $1", ["%#{search_term}%"])
+    format_print_result(results)
+  end
+
+  private
+
+  def format_print_result(pg_result)
+      pg_result.values.each do |value|
+        puts "#{value[0]} | #{value[3]}| #{value[1].rjust(11)}| #{value[2]}"
+      end
+  end
 end
 
-user_command = ARGV
 
-if user_command.first == 'list'
-  list
-else 
-  help
-end
+cli = CLI.new
+cli.run(ARGV)
